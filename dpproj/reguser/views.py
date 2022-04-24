e336ed8517beb92f6e0b8b3eeddbc9ac3d32d167
istomin-constantine <istomin.konstantin123@gmail.com>
2022-04-24 18:14:59 +0300

    Commit

---
+++ dpproj/reguser/views.py
@@ -0,0 +1,9 @@
+from http import client
+from django.shortcuts import render
+
+from django.http import HttpResponse as hres
+from django.views import View
+
+def index(request):
+    client_addr = request.META.get('REMOTE_ADDR')
+    return hres(client_addr)
