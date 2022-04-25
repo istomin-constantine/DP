299120f174c206ce627fa968eaba443199f1ef55
istomin-constantine <istomin.konstantin123@gmail.com>
2022-04-25 18:11:54 +0300

    commit

--- dpproj/reguser/views.py
+++ dpproj/reguser/views.py
@@ -5,9 +5,5 @@ from django.http import HttpResponse as hres
 from django.views import View
 
 def index(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+    #client_addr = request.META.get('REMOTE_ADDR')
+    return hres("HEllo friend")
