3302a1d13907a6b5d6e1d0e6c19e8c1c88847d80
istomin-constantine <istomin.konstantin123@gmail.com>
2022-04-25 18:05:16 +0300

    Update views.py

--- dpproj/reguser/views.py
+++ dpproj/reguser/views.py
@@ -5,5 +5,9 @@ from django.http import HttpResponse as hres
 from django.views import View
 
 def index(request):
-    client_addr = request.META.get('REMOTE_ADDR')
-    return hres(client_addr)
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
