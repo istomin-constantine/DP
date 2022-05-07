c6514ea97785ec7c7689e99705282d79f8249d55
istomin-constantine <istomin.konstantin123@gmail.com>
2022-05-07 15:50:43 +0300

    Commit

--- dpproj/dpproj/settings.py
+++ dpproj/dpproj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*3f)$j!1gk(zn0m5b$z!aprc2ba)dqcacr4h=ian4w1(dxu2&)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','prettyviciousdiploma.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reguser'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- dpproj/reguser/views.py
+++ dpproj/reguser/views.py
@@ -1,10 +1,48 @@
 from http import client
-from django.shortcuts import render
+from threading import Thread
+from urllib import response
 
+import requests
+import speedtest
 from django.http import HttpResponse as hres
+from django.http import JsonResponse
+from django.shortcuts import render
 from django.views import View
 
+
+def testspeed(request):
+    if request.method == 'GET':
+        test  = speedtest.Speedtest()
+        dl = test.download()
+        up = test.upload()
+
+        response = {
+            'dl': format(dl / 1024 / 1024, ".2f"),
+            'up': format(up / 1024 / 1024, ".2f"),
+        }
+        return JsonResponse(response)
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
 def index(request):
-    client_addr = request.META.get('REMOTE_ADDR')
-    return hres(client_addr)
-    #return hres("HEllo friend")
+    client_addr = get_ip()
+
+    response = requests.get(f'https://ipapi.co/{client_addr}/json/').json()
+        
+    data = {
+        'ip': client_addr,
+        'protocol': response.get("version"),
+        'city': response.get("city"),
+        'country_name': response.get("country_name"),
+        'asn': response.get("asn"),
+        'org': response.get("org"),
+        'latitude': response.get("latitude"),
+        'longitude': response.get("longitude"),
+    }
+    return render(request, 'reguser/index.html',data)
+
+def statistics(request):
+    return render(request, 'reguser/statistics.html')
+
